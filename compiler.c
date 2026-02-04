8cd279ebd281021771ea124db9e4103ab3ad87f2
duy4nt <singhduyant@gmail.com>
2026-02-04 22:47:32 +0000

    base for scanner

---
+++ compiler.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "compiler.h"
+#include "scanner.h"
+
+void compiler(const char* source) {
+    initScaner(source);
+
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line;
+            line = token.line;
+        } else {
+            printf("    | ");
+        }
+
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+        if (token.type == TOKEN_EOF) break;
+    }
+}
+
+
--- main.c
+++ main.c
@@ -1,10 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
+static void replt() {
+    char line[1024];
+    
+    for (;;) {
+        printf("> ");
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+        
+        interpret(line);
+    }
+}
+
+static char* readFile(const char* path) {
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) {
+        fprint(stderr, "Unable to open file \"s\".\n", path);
+    }
+    
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char* buffer = (char*)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        fprint(stderr, "Not enough memory");
+        exit(74); 
+    }
+     
+    size_t byteRead = fread(buffer, sizeof(char), fileSize, file);
+    if (byteRead < fileSize) {
+        fprint(stderr, "Could not read file", path);
+        exit(74);
+    }   
+
+    fclose(file);
+    return buffer;
+}
+
+static void runFile(const char* path) {
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+    
+    if (result == INTERPRET_COMPILE_ERROR) exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+
+}
+
 int main(int argc, const char* argv[]) {
     initVM();
+    
+    if (argc == 1) {
+        repl();
+    } else if (argc == 2) {
+        runfile(argv[1]);
+    } else {
+        fprint(stderr, "Usage: clox path");
+        exit(64);
+    }
  
     freeVM();
     return 0;
